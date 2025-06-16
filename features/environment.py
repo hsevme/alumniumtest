151eed12a5fdcfcf9f2b569f3c5a2f3b33977b18
severin.heidrich <severin.heidrich@extern.syncpilot.com>
2025-06-16 09:02:01 +0200

    Add standard playwright cycle

--- features/environment.py
+++ features/environment.py
@@ -1,21 +1,28 @@
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
-from alumnium import Alumni, Model, Provider
+from alumnium import Alumni, Model, Cache
 import os
 from behave.runner import Context
 
 
 def before_all(context: Context):
     load_dotenv()
-    context.browser = sync_playwright().start().chromium.launch(headless=False)
-    print(f'ALUMNIUM_MODEL="{os.environ.get("ALUMNIUM_MODEL")}"')
+    context.playwright = sync_playwright().start()
+    context.browser = context.playwright.chromium.launch(headless=False)
 
-def before_scenario(context, scenario):
+def before_scenario(context):
     context.page = context.browser.new_page()
     context.page.goto("https://autoprojekt.simplytest.de/")
+
     model_provider = os.environ.get("ALUMNIUM_MODEL", "ollama").lower()
     context.al = Alumni(context.page, Model(model_provider))
 
+def after_scenario(context):
+    context.page.close()
+    al: Alumni = context.al
+    cache: Cache = al.cache
+    cache.session.commit()
+
 def after_all(context):
-    context.al.quit()
     context.browser.close()
+    context.playwright.close()
